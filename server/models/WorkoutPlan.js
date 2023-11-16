dd34fa72f8ebb5de94cb36a731eb9d87d57527b6
dmmontoto <dmontoto@ucsd.edu>
2023-11-15 21:11:54 -0800

    added Workout plan object type (holds workouts)

---
+++ server/models/WorkoutPlan.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const workoutPlanSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  workouts: {
+    type: Schema.Types.ObjectId,
+    ref: 'Workout',
+    required: true
+  },
+});
+
+const WorkoutPlan = mongoose.model('WorkoutPlan', workoutPlanSchema);
+
+module.exports = WorkoutPlan;
