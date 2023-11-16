e3bbdb34af71819d56044bb3eea9807911aab4ae
samxbrown <135293566+samxbrown@users.noreply.github.com>
2023-11-15 21:19:52 -0800

    updated title to Tracked Fitness

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -43,7 +43,7 @@ function Nav() {
       <h1>
         <Link to="/">
           <span role="img" aria-label="shopping bag">🏋️</span>
-          Fitness Tracker
+          Tracked Fitness
         </Link>
       </h1>
 
