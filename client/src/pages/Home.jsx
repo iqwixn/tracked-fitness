5c4168ab835fe6a380b5ef7d14ebbb4d9e2a3aa9
Vingrass <ingrassiav99@gmail.com>
2023-11-18 18:25:13 -0800

    added diff messages for login/out

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,29 @@
 
 
 const Home = () => {
-  return (
-    <div className="container">
+  if (Auth.loggedIn()){
+    return (
+      <div className="container">
+            <h1>Welcome!</h1>
+            <p>Your last work out was:    an embarassment, you can do better.</p>
+            <p>"It's not about being the strongest person in the room, it's about being the person that never gave up."</p>
+            <p>-Eddy Hall</p>
+      </div>
+    );
+  }
+  if(!Auth.loggedIn()){
+    return(
+      <div>
+        <h1>Please login or sign up!</h1>
+      </div>
+    )
+  }
 
+  return(
+    <div>
+      
     </div>
-  );
+  )
 };
 
 export default Home;
