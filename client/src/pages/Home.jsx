7a910215aa6c0f3c904967a8adaa7fe0b5a3a64c
dmmontoto <dmontoto@ucsd.edu>
2023-11-20 20:45:54 -0800

    api working

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,42 @@
 import Auth from "../utils/auth";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const [dailyQuote, setDailyQuote] = useState(null);
+
+  const fetchDailyQuote = async () => {
+    if (Auth.loggedIn()) {
+      try {
+        const options = {
+          method: 'GET',
+          url: 'https://quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com/quote',
+          params: {
+            token: 'ipworld.info'
+          },
+          headers: {
+            'X-RapidAPI-Key': '74a73f418emsh8c325d7f91bb73bp128dcajsn3884fc5f12c2',
+            'X-RapidAPI-Host': 'quotes-inspirational-quotes-motivational-quotes.p.rapidapi.com'
+          }
+        };
+
+        const response = await axios.request(options);
+        const newQuote = {
+          text: response.data.text,
+          author: response.data.author,
+        };
+
+        setDailyQuote(newQuote);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchDailyQuote();
+  }, [Auth.loggedIn()]);
+
   if (Auth.loggedIn()){
     return (
       <div className="homeParent">
@@ -9,8 +45,8 @@ const Home = () => {
         <div className="welcomeMessage">
               <h1>Welcome!</h1>
               <p>Your last work out was:    an embarassment, you can do better.</p>
-              <p>"It's not about being the strongest person in the room, it's about being the person that never gave up."</p>
-              <p>-Eddy Hall</p>
+              <p id="Quote">{dailyQuote?.text}</p>
+              <p id="Author">{dailyQuote?.author && `- ${dailyQuote.author}`}</p>
         </div>
         <div className="lastWorkout">
               <h1>Your Last Workout</h1>
